88a96b67184a06db5f75775d22674afe343aa595
Jakub Cieslar <cieslar.jakub@gmail.com>
2014-09-01 15:13:25 +0200

    Add virtual box to tools

--- resources/tools.sh
+++ resources/tools.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list'
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
 
 install terminator
 install guake
 install p7zip
 install p7zip-rar
+install virtualbox-4.3
